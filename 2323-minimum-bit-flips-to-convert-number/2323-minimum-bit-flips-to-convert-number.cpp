4b1c77cf28bc8d0671b3d9344758c28aca43c5c1
JaivalBuchara <98541031+JaivalBuchara@users.noreply.github.com>
2024-09-11 20:04:15 +0530

    Time: 3 ms (41.47%), Space: 7.7 MB (44.87%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -2,16 +2,14 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        int bases = 1,baseg = 1;
-        if(start != 0 ) bases = static_cast<int> (log2(start));
-        if(goal != 0) baseg = static_cast<int> (log2(goal));
+        int differingBits = start ^ goal;
+        
         int count = 0;
-        int f = max(baseg,bases);
-        bitset<30> str(start);
-        bitset<30> gl(goal);
-        for(int i = 0;i<=f;i++){
-            if(str[i] != gl[i]) count++;
+        while (differingBits) {
+            count += differingBits & 1;  
+            differingBits >>= 1;         
         }
+        
         return count;
     }
 };
